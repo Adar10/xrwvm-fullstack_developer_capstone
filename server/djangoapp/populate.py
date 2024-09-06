7c45c2b86b485a4a63a180e31c3ec20bb2c4da2d
Adar10 <adardonmezz@gmail.com>
2024-09-06 19:42:57 -0400

    restored code

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -11,45 +11,25 @@ def initiate():
     ]
     car_make_instances = []
     for data in car_make_data:
-        car_make_instances.append(
-            CarMake.objects.create(
-                name=data['name'],
-                description=data['description']
-            )
-        )
+        car_make_instances.append(CarMake.objects.create(name=data['name'], description=data['description']))
 
     # Create CarModel instances with the corresponding CarMake instances
-    car_model_data = [
-        {"name": "Pathfinder", "type": "SUV", "year": 2023,
-         "car_make": car_make_instances[0], "dealer_id": 101},
-        {"name": "Qashqai", "type": "SUV", "year": 2023,
-         "car_make": car_make_instances[0], "dealer_id": 101},
-        {"name": "XTRAIL", "type": "SUV", "year": 2023,
-         "car_make": car_make_instances[0], "dealer_id": 101},
-        {"name": "A-Class", "type": "SUV", "year": 2023,
-         "car_make": car_make_instances[1], "dealer_id": 102},
-        {"name": "C-Class", "type": "SUV", "year": 2023,
-         "car_make": car_make_instances[1], "dealer_id": 102},
-        {"name": "E-Class", "type": "SUV", "year": 2023,
-         "car_make": car_make_instances[1], "dealer_id": 102},
-        {"name": "A4", "type": "SUV", "year": 2023,
-         "car_make": car_make_instances[2], "dealer_id": 103},
-        {"name": "A5", "type": "SUV", "year": 2023,
-         "car_make": car_make_instances[2], "dealer_id": 103},
-        {"name": "A6", "type": "SUV", "year": 2023,
-         "car_make": car_make_instances[2], "dealer_id": 103},
-        {"name": "Sorrento", "type": "SUV", "year": 2023,
-         "car_make": car_make_instances[3], "dealer_id": 104},
-        {"name": "Carnival", "type": "SUV", "year": 2023,
-         "car_make": car_make_instances[3], "dealer_id": 104},
-        {"name": "Cerato", "type": "Sedan", "year": 2023,
-         "car_make": car_make_instances[3], "dealer_id": 104},
-        {"name": "Corolla", "type": "Sedan", "year": 2023,
-         "car_make": car_make_instances[4], "dealer_id": 105},
-        {"name": "Camry", "type": "Sedan", "year": 2023,
-         "car_make": car_make_instances[4], "dealer_id": 105},
-        {"name": "Kluger", "type": "SUV", "year": 2023,
-         "car_make": car_make_instances[4], "dealer_id": 105},
+   car_model_data = [
+        {"name": "Pathfinder", "type": "SUV", "year": 2023, "car_make": car_make_instances[0], "dealer_id": 101},
+        {"name": "Qashqai", "type": "SUV", "year": 2023, "car_make": car_make_instances[0], "dealer_id": 101},
+        {"name": "XTRAIL", "type": "SUV", "year": 2023, "car_make": car_make_instances[0], "dealer_id": 101},
+        {"name": "A-Class", "type": "SUV", "year": 2023, "car_make": car_make_instances[1], "dealer_id": 102},
+        {"name": "C-Class", "type": "SUV", "year": 2023, "car_make": car_make_instances[1], "dealer_id": 102},
+        {"name": "E-Class", "type": "SUV", "year": 2023, "car_make": car_make_instances[1], "dealer_id": 102},
+        {"name": "A4", "type": "SUV", "year": 2023, "car_make": car_make_instances[2], "dealer_id": 103},
+        {"name": "A5", "type": "SUV", "year": 2023, "car_make": car_make_instances[2], "dealer_id": 103},
+        {"name": "A6", "type": "SUV", "year": 2023, "car_make": car_make_instances[2], "dealer_id": 103},
+        {"name": "Sorrento", "type": "SUV", "year": 2023, "car_make": car_make_instances[3], "dealer_id": 104},
+        {"name": "Carnival", "type": "SUV", "year": 2023, "car_make": car_make_instances[3], "dealer_id": 104},
+        {"name": "Cerato", "type": "Sedan", "year": 2023, "car_make": car_make_instances[3], "dealer_id": 104},
+        {"name": "Corolla", "type": "Sedan", "year": 2023, "car_make": car_make_instances[4], "dealer_id": 105},
+        {"name": "Camry", "type": "Sedan", "year": 2023, "car_make": car_make_instances[4], "dealer_id": 105},
+        {"name": "Kluger", "type": "SUV", "year": 2023, "car_make": car_make_instances[4], "dealer_id": 105},
     ]
     for data in car_model_data:
         CarModel.objects.create(
--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -17,18 +17,20 @@ sentiment_analyzer_url = os.getenv(
 # Add code for get requests to back end
 def get_request(endpoint, **kwargs):
     params = ""
-    if kwargs:
-        for key, value in kwargs.items():
-            params += key + "=" + value + "&"
-    request_url = backend_url + endpoint + "?" + params
+    if(kwargs):
+        for key,value in kwargs.items():
+            params=params+key+"="+value+"&"
+
+    request_url = backend_url+endpoint+"?"+params
+
     print("GET from {} ".format(request_url))
     try:
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
-    except Exception as e:
-        # Handle any exception that occurs
-        print(f"Exception occurred: {e}")
+    except Exception:
+        # If any error occurs
+        print("Network exception occurred")
 
 
 # def analyze_review_sentiments(text):
@@ -37,7 +39,7 @@ def get_request(endpoint, **kwargs):
 
 # Add code for retrieving sentiments
 def analyze_review_sentiments(text):
-    request_url = sentiment_analyzer_url + "analyze/" + text
+    request_url = sentiment_analyzer_url+"analyze/"+text
     try:
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
@@ -50,10 +52,10 @@ def analyze_review_sentiments(text):
 
 # Add code for posting review
 def post_review(data_dict):
-    request_url = backend_url + "/insert_review"
+    request_url = backend_url+"/insert_review"
     try:
-        response = requests.post(request_url, json=data_dict)
+        response = requests.post(request_url,json=data_dict)
         print(response.json())
         return response.json()
-    except Exception as e:
-        print(f"Exception occurred: {e}")
+    except:
+        print("Network exception occurred")
