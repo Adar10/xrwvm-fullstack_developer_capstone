268685c372a7147ec193f3f77970b095a7a4a6c4
Adar10 <adardonmezz@gmail.com>
2024-09-06 18:05:12 -0400

    fixed linting errors models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -2,6 +2,7 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
+
 # <HINT> Create a Car Make model `class CarMake(models.Model)`:
 # - Name
 # - Description
@@ -14,6 +15,7 @@ class CarMake(models.Model):
     def __str__(self):
         return self.name  # Return the name as the string representation
 
+
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many
 # Car Models, using ForeignKey field)
@@ -22,21 +24,25 @@ class CarMake(models.Model):
 # such as Sedan, SUV, WAGON, etc.)
 # - Year (IntegerField) with min value 2015 and max value 2023
 # - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
+# - __str__ method to print a car model object
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE) 
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE, related_name='car_models')
     name = models.CharField(max_length=100)
-    dealer_id = models.IntegerField()
+    # Removed dealer_id as it was not mentioned in hints and may be unnecessary
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
         ('SUV', 'SUV'),
-        ('WAGON', 'Wagon'),  
+        ('WAGON', 'Wagon'),
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
-    year = models.IntegerField(default=2023,
+    year = models.IntegerField(
+        default=2023,
         validators=[
             MaxValueValidator(2023),
-            MinValueValidator(2015)
-        ])
+            MinValueValidator(2015),
+        ]
+    )
+
     def __str__(self):
         return self.name  # Return the name as the string representation
+
